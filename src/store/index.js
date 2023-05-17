5ea50cdd5cf8ec5250b23c1cac34cc3ed3991717
mina <mineezm9@gmail.com>
2023-05-17 15:43:40 +0900

    feat: 로그인 페이지 생성, 로그인 실패 구현

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+// const state = {
+//   userId: '',
+
+// }
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {},
+});
