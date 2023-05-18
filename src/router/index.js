346d786605cbebc363137b0b3da9c28f8658c2af
mina <mineezm9@gmail.com>
2023-05-18 13:31:24 +0900

    feat: 로그인 하지 않고 페이지 이동하는 경우 로그인 페이지로 이동
    로그인 하지 않고 페이지 이동하는 경우 로그인 페이지로 이동하도록 구현
    메인, 로그인, 회원가입 페이지를 제외한 모든 페이지에 적용

--- src/router/index.js
+++ src/router/index.js
@@ -11,69 +11,88 @@ import BoardPage from "@/views/Board";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "main",
-    component: MainPage,
-  },
-  {
-    path: "/attraction",
-    name: "attraction",
-    component: AttractionPage,
-  },
-  {
-    path: "/board",
-    name: "board",
-    component: BoardPage,
-    redirect: "/board/search",
-    children: [
-      {
-        path: "search",
-        name: "boardList",
-        component: () => import("@/components/board/BoardList.vue"),
-      },
-      {
-        path: "write",
-        name: "boardWriter",
-        component: () =>
-          import("@/components/board/BoardWriter.vue"),
-      },
-      {
-        path: "view/:articleNo",
-        name: "boardDetail",
-        component: () =>
-          import("@/components/board/BoardDetail.vue"),
-      },
-      {
-        path: "modify/:articleNo",
-        name: "boardModify",
-        component: () =>
-          import("@/components/board/BoardModify.vue"),
-      },
-      {
-        path: "delete/:articleNo",
-        name: "boardDelete",
-        component: () =>
-          import("@/components/board/BoardDelete.vue"),
-      },
-    ],
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: LoginPage,
-  },
-  {
-    path: "/user/join",
-    name: "join",
-    component: JoinPage,
-  },
+    {
+        path: "/",
+        name: "main",
+        component: MainPage,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/attraction",
+        name: "attraction",
+        component: AttractionPage,
+        meta: { requiresAuth: true },
+    },
+    {
+        path: "/board",
+        name: "board",
+        component: BoardPage,
+        redirect: "/board/search",
+        children: [
+            {
+                path: "search",
+                name: "boardList",
+                component: () => import("@/components/board/BoardList.vue"),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: "write",
+                name: "boardWriter",
+                component: () => import("@/components/board/BoardWriter.vue"),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: "view/:articleNo",
+                name: "boardDetail",
+                component: () => import("@/components/board/BoardDetail.vue"),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: "modify/:articleNo",
+                name: "boardModify",
+                component: () => import("@/components/board/BoardModify.vue"),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: "delete/:articleNo",
+                name: "boardDelete",
+                component: () => import("@/components/board/BoardDelete.vue"),
+                meta: { requiresAuth: true },
+            },
+        ],
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: LoginPage,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/user/join",
+        name: "join",
+        component: JoinPage,
+        meta: { requiresAuth: false },
+    },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
+
+router.beforeEach(function (to, _, next) {
+    const access_token = localStorage.getItem("accessToken");
+    if (!access_token) {
+        if (to.meta.requiresAuth) {
+            alert("로그인 후 이용해주세요.");
+            next("/login");
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
