3626ed95756906e030d48e7c8414c66cb8c35913
mina <mineezm9@gmail.com>
2023-05-19 15:13:40 +0900

    fix: 헤더 인증 오류 수정
    헤더의 Authorization에 액세스 토큰이 안담기는 오류 발생함
    원래 axios의 객체가 생성될 때 로컬스토리지에서 액세스 토큰을 가져오도록 구현했었음
    그래서 로그인/로그아웃 때 헤더에 제대로 안담김
    로그인/로그아웃 될때마다 axios 객체 해더에 접근하여 토큰을 추가/삭제하도록 수정

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,54 +1,57 @@
 import axios from "@/util/axios";
 
 let setUserId = ({ commit }, data) => {
-    commit("UserId", data);
+  commit("UserId", data);
 };
 
 let setUserNo = ({ commit }, data) => {
-    commit("UserNo", data);
+  commit("UserNo", data);
 };
 
 let setUserName = ({ commit }, data) => {
-    commit("UserName", data);
+  commit("UserName", data);
 };
 
 // 백엔드에서 반환한 결과값을 가지고 로그인 성공 실패 여부를 vuex에 넣어준다.
 let processResponse = (store, response) => {
-    setUserId(store, response.userId);
-    setUserNo(store, response.userNo);
-    setUserName(store, response.userName);
-    localStorage.setItem("accessToken", response.token.accessToken);
-    localStorage.setItem("refreshToken", response.token.refreshToken);
+  setUserId(store, response.userId);
+  setUserNo(store, response.userNo);
+  setUserName(store, response.userName);
+  // localStorage.setItem('user',JSON.stringify(userData));
+  axios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${response.token.accessToken}`;
+  localStorage.setItem("accessToken", response.token.accessToken);
+  localStorage.setItem("refreshToken", response.token.refreshToken);
 };
 
 const loginApi = async (store, { userId, password }) => {
-    axios
-        .post(`/auth/check`, {
-            userId: userId,
-            password: password,
-        })
-        .then((response) => {
-            console.log("before", store);
-            processResponse(store, response.data.data);
+  axios
+    .post(`/auth/check`, {
+      userId: userId,
+      password: password,
+    })
+    .then((response) => {
+      processResponse(store, response.data.data);
+    });
 
-            console.log("after", store);
-        });
-
-    return store.getters.getUserId; // 로그인 결과를 리턴한다
+  return store.getters.getUserId; // 로그인 결과를 리턴한다
 };
 
 const logoutApi = async (store) => {
-    console.log("logoutApi");
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-    setUserId(store, null);
-    setUserNo(store, null);
-    setUserName(store, null);
+  console.log("logoutApi");
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
+  setUserId(store, null);
+  setUserNo(store, null);
+  setUserName(store, null);
 
-    console.log(store);
+  delete axios.defaults.headers.common["Authorization"];
+
+  console.log(store);
 };
 
 export default {
-    loginApi,
-    logoutApi,
+  loginApi,
+  logoutApi,
 };
