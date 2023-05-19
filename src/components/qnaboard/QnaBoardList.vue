c11abd755223a6871fc37cfc19aea0c1aabf2103
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-05-19 12:51:17 +0900

    feat: QnA 게시판 리스트 화면 Vuetify 적용

--- src/components/qnaboard/QnaBoardList.vue
+++ src/components/qnaboard/QnaBoardList.vue
@@ -1,29 +1,46 @@
 <template>
-  <v-container>
-    <v-card class="mb-4">
-      <v-card-title primary-title>Q&A 게시판</v-card-title>
-    </v-card>
-    <v-text-field
-      v-model="search"
-      label="Search"
-      solo
-      single-line
+  <v-container class="d-flex flex-column">
+    <v-sheet
+      class="d-flex flex-row align-center align-self-center justify-center ma-1"
+      color="white"
+      height="50px"
+      width="100%"
     >
-    </v-text-field>
-    <div v-if="articles.length">
+      <v-sheet width="150px">
+        <v-select
+          v-model="searchKey"
+          hide-details="true"
+          label="옵션"
+          :items="searchCondition"
+          solo
+        >
+        </v-select>
+      </v-sheet>
+      <v-text-field
+        class="ml-3 mr-3 align-center"
+        hide-details="true"
+        v-model="search"
+        label="Search"
+        solo
+        single-line
+      >
+      </v-text-field>
+      <v-btn elevation="2" height="50px" width="100px" @click="pressSearch()"
+        >검색</v-btn
+      >
+    </v-sheet>
+    <div class="d-flex flex-column" v-if="articles.length">
       <v-data-table
+        class="align-self-center"
         style="width: 100%"
         :headers="headers"
         :items="articles"
         :items-per-page="10"
-        :search="search"
         @click:row="openDetail"
       ></v-data-table>
     </div>
     <div v-else>
-      <v-text-field v-model="search" label="Search" solo single-line>
-      </v-text-field>
-      <div v-if="articles.length">
+      <div class="d-flex flex-column" v-if="articles.length">
         <v-data-table
           style="width: 100%"
           :headers="headers"
@@ -34,17 +51,25 @@
         ></v-data-table>
       </div>
     </div>
-    <v-btn color="#A4D9FF" @click="moveWrite()">등록</v-btn>
+    <v-btn
+      class="align-self-end"
+      width="50px"
+      color="#A4D9FF"
+      @click="moveWrite()"
+      >등록</v-btn
+    >
   </v-container>
 </template>
 
 <script>
 import axios from "@/util/axios";
+import { mapGetters } from "vuex";
 
 export default {
   name: "QnaBoardList",
   components: {},
   data: () => ({
+    searchKey: "",
     search: "",
     headers: [
       {
@@ -59,7 +84,7 @@ export default {
         value: "subject",
         sortable: false,
         width: "55%",
-        align: "end",
+        align: "start",
       },
       {
         text: "작성자",
@@ -93,15 +118,30 @@ export default {
         readCount: "",
       },
     ],
+    searchCondition: [
+      {
+        text: "제목",
+        value: "subject",
+      },
+      {
+        text: "작성자",
+        value: "userName",
+      },
+      {
+        text: "내용",
+        value: "content",
+      },
+    ],
   }),
   created() {
     axios({
       url: "/qna/search",
       method: "post",
       data: JSON.stringify({
-        userNo: "2",
+        userNo: this.userNo,
       }),
     }).then(({ data }) => {
+      console.log(data);
       data.data.forEach((element) => {
         if (element.registerTime == "") return "";
 
@@ -119,7 +159,6 @@ export default {
         }
 
         element.registerTime = year + "-" + month + "-" + date;
-        console.log(element.registerTime);
       });
 
       data.data.forEach((element) => {
@@ -139,14 +178,67 @@ export default {
         }
 
         element.modifiedTime = year + "-" + month + "-" + date;
-        console.log(element.modifiedTime);
       });
 
       this.articles = data.data;
-      console.log(data);
     });
   },
   methods: {
+    pressSearch() {
+      if (this.searchKey == "") {
+        alert("검색 옵션을 선택해 주세요");
+        return;
+      }
+
+      axios({
+        url: `/qna/search?key=${this.searchKey}&value=${this.search}`,
+        method: "post",
+        data: JSON.stringify({
+          userNo: this.userNo,
+        }),
+      }).then(({ data }) => {
+        console.log(data);
+        data.data.forEach((element) => {
+          if (element.registerTime == "") return "";
+
+          let jsDate = new Date(element.registerTime);
+
+          let year = jsDate.getFullYear();
+          let month = jsDate.getMonth() + 1;
+          let date = jsDate.getDate();
+
+          if (month < 10) {
+            month = "0" + month;
+          }
+          if (date < 10) {
+            date = "0" + date;
+          }
+
+          element.registerTime = year + "-" + month + "-" + date;
+        });
+
+        data.data.forEach((element) => {
+          if (element.modifiedTime == "") return "";
+
+          let jsDate = new Date(element.modifiedTime);
+
+          let year = jsDate.getFullYear();
+          let month = jsDate.getMonth() + 1;
+          let date = jsDate.getDate();
+
+          if (month < 10) {
+            month = "0" + month;
+          }
+          if (date < 10) {
+            date = "0" + date;
+          }
+
+          element.modifiedTime = year + "-" + month + "-" + date;
+        });
+
+        this.articles = data.data;
+      });
+    },
     moveWrite() {
       this.$router.push({ name: "qnaBoardWriter" });
     },
@@ -156,6 +248,9 @@ export default {
         params: { articleNo: val.articleNo },
       });
     },
+  },
+  computed: {
+    ...mapGetters({ userNo: "getUserNo" }),
   },
 };
 </script>
