a8eed49c35c2b7926eabe8151a5aa1892311eaf4
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-05-19 17:55:35 +0900

    test: 테스트 코드 삭제

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,13 +1,12 @@
 <template>
-    <div>
-        여기 게시판 만들어야함
-        <router-view></router-view>
-    </div>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "BoardPage",
+  name: "BoardPage",
 };
 </script>
 
