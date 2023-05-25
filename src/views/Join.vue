cb4f99cca844f43cd81ee067437605c964fbfede
Mina Park <mineezm9@gmail.com>
2023-05-26 00:05:51 +0900

    fix: 회원가입 시 정보 유효성 검사 강화

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -11,43 +11,32 @@
                   <v-text-field
                     label="아이디"
                     placeholder="아이디를 입력하세요."
+                    :rules="userIdRule"
                     v-model="userId"
                   ></v-text-field>
-                  <div v-show="userIdErr" class="input-error">
-                    아이디는 4자리 이상, 20자리 이하로 영대소문자, 숫자를 사용할
-                    수 있으며 숫자로 시작할 수 없습니다.
-                  </div>
                   <v-text-field
                     label="이름"
                     placeholder="이름을 입력하세요."
+                    :rules="userNameRule"
                     v-model="userName"
                   >
                   </v-text-field>
-                  <div v-show="userNameErr" class="input-error">
-                    이름은 2자리 이상, 15자리 이하로 영대소문자, 한글, 숫자를
-                    사용할 수 있습니다.
-                  </div>
                   <v-text-field
                     type="password"
                     label="비밀번호"
+                    :rules="passwordRule"
                     placeholder="비밀번호를 입력하세요."
                     v-model="password"
                   >
                   </v-text-field>
-                  <div v-show="passwordErr" class="input-error">
-                    비밀번호는 8자리 이상, 16자리 이하의 영문자, 숫자,
-                    특수문자가 포함되어야 합니다.
-                  </div>
                   <v-text-field
                     type="email"
                     label="이메일"
+                    :rules="emailRule"
                     placeholder="이메일을 입력하세요."
                     v-model="email"
                   >
                   </v-text-field>
-                  <div v-show="emailErr" class="input-error">
-                    이메일이 형식에 맞지 않습니다.
-                  </div>
                   <v-btn
                     color="blue lighten-1 text-capitalize"
                     depressed
@@ -60,6 +49,20 @@
                     회원가입
                   </v-btn>
                 </form>
+                <v-snackbar v-model="snackbar" :timeout="timeout">
+                  {{ text }}
+
+                  <template v-slot:action="{ attrs }">
+                    <v-btn
+                      color="blue"
+                      text
+                      v-bind="attrs"
+                      @click="snackbar = false"
+                    >
+                      Close
+                    </v-btn>
+                  </template>
+                </v-snackbar>
               </div>
             </v-card>
           </v-flex>
@@ -91,21 +94,67 @@ export default {
       text: "",
       snackbar: false,
       timeout: 2000,
+      userNameRule: [
+        (v) => !!v.trim() || "이름은 필수 입력사항입니다.",
+        (v) => !(v && v.length >= 30) || "이름은 30자 이상 입력할 수 없습니다.",
+        (v) =>
+          !/[~!@#$%^&*()_+|<>?:{}]/.test(v) ||
+          "이름에는 특수문자를 사용할 수 없습니다.",
+      ],
+      userIdRule: [
+        (v) => !!v.trim() || "아이디는 필수 입력사항입니다.",
+        (v) =>
+          /^[a-zA-Z][0-9a-zA-Z]{3,19}$/.test(v) ||
+          "아이디는 4자리 이상, 20자리 이하로 영대소문자, 숫자를 사용할 수 있으며 숫자로 시작할 수 없습니다.",
+      ],
+      passwordRule: [
+        (v) => !!v.trim() || "비밀반호는 필수 입력사항입니다.",
+        (v) =>
+          /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/.test(
+            v
+          ) ||
+          "비밀번호는 8자리 이상, 16자리 이하의 영문자, 숫자, 특수문자가 포함되어야 합니다.",
+      ],
+      emailRule: [
+        (v) => !!v.trim() || "이메일은 필수 입력사항입니다.",
+        (v) =>
+          /^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(v) ||
+          "이메일이 형식에 맞지 않습니다.",
+      ],
     };
   },
   created() {},
   methods: {
     join() {
-      if (this.userIdErr) {
+      if (
+        this.userId.trim() == "" ||
+        (this.userId && this.userId.length < 4) ||
+        (this.userId && this.userId.length > 20) ||
+        !/^[a-zA-Z][0-9a-zA-Z]{3,19}$/.test(this.userId)
+      ) {
         this.text = this.userIdText;
         this.snackbar = true;
-      } else if (this.userNameErr) {
+      } else if (
+        this.userName.trim() == "" ||
+        (this.userName && this.userName.length >= 30) ||
+        /[~!@#$%^&*()_+|<>?:{}]/.test(this.userName)
+      ) {
         this.text = this.userNameText;
         this.snackbar = true;
-      } else if (this.passwordErr) {
+      } else if (
+        this.password.trim() == "" ||
+        (this.password && this.password.length > 16) ||
+        (this.password && this.password.length < 8) ||
+        !/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/.test(
+          this.password
+        )
+      ) {
         this.text = this.passwordText;
         this.snackbar = true;
-      } else if (this.emailErr) {
+      } else if (
+        this.email.trim() == "" ||
+        !/^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(this.email)
+      ) {
         this.text = this.emailText;
         this.snackbar = true;
       } else {
@@ -115,69 +164,20 @@ export default {
             userName: this.userName,
             password: this.password,
             emailId: this.email.substring(0, this.email.indexOf("@")),
-            emailDomain: this.email.substring(this.email.indexOf("@")+1),
+            emailDomain: this.email.substring(this.email.indexOf("@") + 1),
           })
           .then((response) => {
             console.log(response.data);
             this.$router.push("/");
+          })
+          .catch(() => {
+            this.text = "회원가입 중 오류가 발생하였습니다.";
+            this.snackbar = true;
           });
       }
-    },
-    checkUserId() {
-      const reg = /^[a-zA-Z][0-9a-zA-Z]{3,19}$/;
-      if (!reg.test(this.userId) || !this.userId) {
-        this.userIdErr = true;
-        console.log("checkUserId");
-      } else {
-        this.userIdErr = false;
-      }
-    },
-    checkUserName() {
-      const reg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,15}$/;
-      if (!reg.test(this.userName) || !this.userName) {
-        this.userNameErr = true;
-      } else {
-        this.userNameErr = false;
-      }
-    },
-    checkPassword() {
-      const reg =
-        /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
-      if (!reg.test(this.password) || !this.password) {
-        this.passwordErr = true;
-      } else {
-        this.passwordErr = false;
-      }
-    },
-    checkEmail() {
-      const reg = /^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-      if (!reg.test(this.email) || !this.email) {
-        this.emailErr = true;
-      } else {
-        this.emailErr = false;
-      }
-    },
-  },
-  watch: {
-    password: function () {
-      this.checkPassword();
-    },
-    userId: function () {
-      this.checkUserId();
-    },
-    userName: function () {
-      this.checkUserName();
-    },
-    email: function () {
-      this.checkEmail();
     },
   },
 };
 </script>
 
-<style scoped>
-.input-error {
-  font-size: 11px;
-  color: red;
-}
-</style>
+<style scoped></style>
