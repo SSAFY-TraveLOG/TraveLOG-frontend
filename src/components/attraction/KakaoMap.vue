70227e67c84388bdae8728f3700d3eceecd65dfc
Lee-Jiseung <ljss98@naver.com>
2023-05-25 10:36:07 +0900

    fix: 카카오맵 로드 오류 수정

--- src/components/attraction/KakaoMap.vue
+++ src/components/attraction/KakaoMap.vue
@@ -14,10 +14,8 @@ export default {
       markers: [],
     }
   },
-  mounted() {
-    if (!window.kakao || !window.kakao.maps) {
-      this.loadScript();
-    }
+  created() {
+    this.loadScript();
   },
   methods: {
     loadScript() {
@@ -40,6 +38,8 @@ export default {
         this.loadMarker(attractions.slice(0, Math.min(attractions.length, 10)));
     },
     loadMarker(attractions) {
+      console.log(`loadMarker`)
+      console.log(attractions.length)
       this.markers.forEach((marker) => {
         marker.setMap(null);
       });
@@ -60,6 +60,7 @@ export default {
         marker.setMap(this.map);
         this.markers.push(marker);
       });
+      console.log(positions)
       this.moveCenter(positions[0].latlng.getLat(), positions[0].latlng.getLng());
     },
     moveCenter(lat, lng) {
